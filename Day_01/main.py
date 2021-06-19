08be875b45c0d0611e8297ada68f2cca3512c0c6
Mark <bakonyimark8@gmail.com>
2021-06-19 17:44:58 +0200

    the end of the day01 session

--- Day_01/main.py
+++ Day_01/main.py
@@ -1,1 +1,37 @@
-print("valami")
+"""
+OUTPUT
+"""
+#print with double commas
+print("Hello World!")
+
+#print with solo comma
+print('Hello there!')
+
+#combine
+print("This is a 'paragraph'")
+
+#combine or
+print('This is another "paragraph"')
+
+#use print with tabs
+print("Here is \t a tab")
+
+#use print with break lines
+print("I used \n ")
+
+#print with concats
+print("Hello" +" "+ "Josh")
+
+"""
+INPUT
+"""
+input("What is your name?")
+
+#Hello with input inline
+print("Hello"+" "+ input("What is your name? ")+" "+"!")
+
+#print the length of the input
+print(len(input("Type a word here... ")))
+
+#set variable by input
+gNumber = input("Guess my favourite number = ")
---
+++ Day_01/project.py
@@ -0,0 +1,7 @@
+"""
+Summary: Day_01 Project
+    Given the user first and second name and the program parse them together. It will be the user's band name.
+"""
+
+#in one line
+print("Your band name: "+input("What is your first name : ")+" "+input("What is your second name : "))
